bf219964dfb08efc4e00bf62dce0f562cbab4058
Linetes <linetescesar@gmail.com>
2021-08-31 02:10:45 -0500

    Groups module

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class GroupMembersInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your views here.
